738d13d46669db2788e6e6fde9047f325879a08b
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-30 00:28:52 -0800

    uploading image assets. Created array of objects for data compomparisons.

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,5 +1,18 @@
+var path = require('path');
 var server = require(./server.js)
+
+module.exports = function(app) {
 
 app.get("/", function(req, res){
     res.send("Welcome to Friend Finder!")
+});
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
 })
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+})
+
+};
