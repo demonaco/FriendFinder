a86592d8d571b66096fa47ac369929351d513e79
Matt DeMonaco <mdemonaco@gmail.com>
2020-01-06 22:10:52 -0800

    pushing up read.me .gifs and screenshots

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FriendFinder
 An Express app to help you find people with similar interests
+
+## Summary:
+The Friend Finder app is the go-to app to find your new friend! Answer a survey of ten questions and the backend logic will find a new friend for you. The application is meant to showcase my learning of Express.js, routing, and setting up/using a local server. The app is also an experimentation in discovering what can be accomplished with API data and how objects and array data can be used to accomplish logistical goals.
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,24 +2,13 @@ var friendsData = require('../app/data/friends');
 
 module.exports = function (app) {
 
-    app.get("/api/friends", function (req, res){
+    app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     })
 
-app.post("/api/friends", function (req, res) {
-    friendsData.push(req.body);
-    res.json(true);
-    console.log(friendsData)
-});
+    app.post("/api/friends", function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+        console.log(friendsData)
+    });
 }
-
-//on click listener sends user data to friends.js, friends.js exports to apiRoutes, use API routes to create logic for comparison. After comparison is done append friend data to modal.
-
-//.abs look up for absolute value parseInt to wrap up in .abs to include negatives
-
-//Clear out friends data if needed
-
-// app.put("/api/clear", function(req, res) {
-// friendsData.length = [];
-// res.json({ ok: true });
-// })
